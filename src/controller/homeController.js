94deea55ac46267428ba97bbb8c24f9f20f7cf83
Mauricio Andreolla de Brito <mauricioandreolla0000@gmail.com>
2022-04-29 22:52:58 -0300

    Inserir filmes em cartaz
    Inserir filmes em cartaz

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,13 +1,24 @@
+const Schedule = require('../model/Schedule');
+const Movie = require('../model/Movie');
+const Image = require('../model/Image');
+
+
 module.exports = {
 
-
     async index(request, response) {
-        return response.render('home/index', { title: "Página Inicial" });
+
+        const movieList = [];
+        const scheduleList = await Schedule.findAll({
+            include: 'Movie',
+        });
+
+        for (let index = 0; index < scheduleList.length; index++) {
+            movieList[index] = await Movie.findByPk(scheduleList[index].Movie.id, {
+                include: Image,
+            });
+        }
+
+        return response.render('home/index', { title: "Página Inicial", scheduleList: scheduleList, movieList: movieList });
     },
-
-  
-
-
-  
 
 }
