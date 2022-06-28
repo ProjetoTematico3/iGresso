ec42d09b145183e85b3b31b628530f02820b5be7
susinLeo <90356516+susinLeo@users.noreply.github.com>
2022-06-27 22:52:16 -0300

    atualização para cadastro de funcionario
    preciso verificar que tem algo errado

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -24,7 +24,13 @@ module.exports = {
         return response.render('login/signup', { title: "Registre-se" });
     },
 
+    async employee(request, response) {
+
+        return response.render('login/employee', { title: "Cadastrar funcionario" });
+    },
+    
     async register(request, response) {
+        
         const params = request.body
         
         if(params.senha != params.repsenha)
@@ -48,7 +54,36 @@ module.exports = {
 
         return response.redirect("/login");
 
-    }
+    },
+
+    
+
+    // async contract(request, response) {
+        
+    //     const params = request.body
+        
+    //     if(params.senha != params.repsenha)
+    //         return response.render('login/employee', { title: "Cadastrar funcionario", fail: true, error_message: "Senhas não coincidem" });
+        
+    //     const check_user = await UserModel.findAll({
+    //         where: {
+    //             email: params.email
+    //         }
+    //     });
+
+    //     if(check_user.length > 0) 
+    //         return response.render('login/employee', { title: "Cadastrar funcionario", fail: true, error_message: "E-mail já registrado!" });
+        
+    //     await UserModel.create({
+    //         email: params.email.toLowerCase(),
+    //         nome: params.name,
+    //         senha: await hash(params.senha, 8),
+    //         tipo_usuario: 2
+    //     });
+
+    //     return response.redirect("/admin");
+
+    // }
 
    
 
